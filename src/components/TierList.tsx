2e425551ac8285141e460ad39013490d9072560f
Nir Tamir <nirtamir2@gmail.com>
2024-10-07 15:11:49 +0300

    add shadow

--- src/components/TierList.tsx
+++ src/components/TierList.tsx
@@ -3,40 +3,57 @@ import { For } from "solid-js";
 const tiers = [
   {
     id: "S",
-    color: "from-red-600 to-red-500",
+    color: "#DC2626",
     items: ["Item 1", "Item 2", "Item 3"],
   },
   {
     id: "A",
-    color: "from-orange-600 to-orange-500",
+    color: "#EA580D",
     items: ["Item 4", "Item 5", "Item 6"],
   },
   {
     id: "B",
-    color: "from-yellow-600 to-yellow-500",
-    items: ["Item 7", "Item 8"],
+    color: "#CA8A05",
+    items: [
+      "Item 7",
+      "Item 8",
+      "Item 81",
+      "Item 82",
+      "Item 7",
+      "Item 8",
+      "Item 81",
+      "Item 82",
+    ],
   },
   {
     id: "C",
-    color: "from-green-600 to-green-500",
-    items: ["Item 9", "Item 10"],
+    color: "#12A34A",
+    items: ["Item 9", "Item 10", "#12A34A"],
   },
-  { id: "D", color: "from-blue-600 to-blue-500", items: ["Item 11"] },
-  { id: "F", color: "from-purple-600 to-purple-500", items: ["Item 12"] },
+  { id: "D", color: "#2563EB", items: ["Item 11"] },
+  { id: "F", color: "#9333EA", items: ["Item 12"] },
 ];
 
 export function TierList() {
   return (
-    <ul class="space-y-2">
+    <ul class="flex flex-col gap-2">
       <For each={tiers}>
         {(tier) => (
           <li class="flex">
             <div
-              class={`flex size-20 items-center justify-center bg-gradient-to-r text-2xl font-bold ${tier.color} rounded-l-lg`}
+              class="flex min-h-32 min-w-16 items-center justify-center rounded-l-lg text-2xl font-bold sm:min-w-32"
+              style={{
+                "background-image": `linear-gradient(to right, ${tier.color}, color-mix(in lch, ${tier.color}, transparent 10%))`,
+              }}
             >
               {tier.id}
             </div>
-            <div class="grow rounded-r-lg bg-gradient-to-r from-gray-800 to-gray-700 p-2">
+            <div
+              class="grow rounded-r-lg bg-gradient-to-r from-gray-800 to-gray-700 p-2"
+              style={{
+                "box-shadow": `inset 0 2px 4px 0 color-mix(in lch, ${tier.color}, transparent 10%)`,
+              }}
+            >
               <div class="flex flex-wrap gap-2">
                 <For each={tier.items}>
                   {(item) => (
