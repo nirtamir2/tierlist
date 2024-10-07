df9866d6034fcf9aafda1a7d3c38f9e4da91368a
Nir Tamir <nirtamir2@gmail.com>
2024-10-07 16:13:44 +0300

    styles and background

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -6,8 +6,12 @@ import { TierList } from "../components/TierList";
 const Home: VoidComponent = () => {
   const tiers = helloQuery(() => ({ hello: "from pRPC" }));
   return (
-    <div class="min-h-screen bg-gradient-to-br from-gray-900 to-gray-800 p-6 text-white">
-      <h1 class="mb-6 text-center text-3xl font-bold">Tier List</h1>
+    <div class="flex flex-col gap-6 p-6">
+      <div class="flex w-full items-center justify-center">
+        <h1 class="inline-block bg-gradient-to-br from-gray-200 to-gray-500 bg-clip-text text-6xl font-bold text-transparent">
+          Tier List
+        </h1>
+      </div>
       <Show when={tiers.data}>
         {(data) => {
           return <TierList tiers={data()} />;
