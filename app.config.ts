1348987c9162130746a766318660218e50e0a120
Nir Tamir <nirtamir2@gmail.com>
2024-10-14 09:26:35 +0300

    remove unused deps

--- app.config.ts
+++ app.config.ts
@@ -1,11 +1,7 @@
-import { prpcVite } from "@solid-mediakit/prpc-plugin";
 import { defineConfig } from "@solidjs/start/config";
 
 export default defineConfig({
   ssr: true,
-  vite: {
-    plugins: [prpcVite()],
-  },
   server: {
     preset: "vercel",
   },
--- src/app.tsx
+++ src/app.tsx
@@ -1,24 +1,17 @@
 // @refresh reload
-import { PRPCProvider } from "@solid-mediakit/prpc/provider";
 import { MetaProvider, Title } from "@solidjs/meta";
 import { Router } from "@solidjs/router";
 import { FileRoutes } from "@solidjs/start/router";
-import { QueryClient } from "@tanstack/solid-query";
 import { Suspense } from "solid-js";
 import "./app.css";
 
 export default function App() {
-  const queryClient = new QueryClient();
   return (
     <Router
       root={(props) => (
         <MetaProvider>
-          <Title>Create JD App</Title>
-          <Suspense>
-            <PRPCProvider queryClient={queryClient}>
-              {props.children}
-            </PRPCProvider>
-          </Suspense>
+          <Title>Tierlist</Title>
+          <Suspense>{props.children}</Suspense>
         </MetaProvider>
       )}
     >
