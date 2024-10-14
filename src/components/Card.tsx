b73c53dd31899e9e021d64f9045721a1341122c1
Nir Tamir <nirtamir2@gmail.com>
2024-10-15 00:38:07 +0300

    card

--- src/components/Card.tsx
+++ src/components/Card.tsx
@@ -6,12 +6,16 @@ interface Props {
 }
 export function Card(props: Props) {
   return (
-    <div class="group/card relative flex h-36 flex-col justify-between rounded-md p-4 shadow-inner shadow-white">
-      <div class="absolute left-0 top-0 size-full transition duration-300 group-hover/card:bg-white/20" />
+    <div class="group/card relative flex h-36 flex-col justify-between overflow-hidden rounded-md border border-gray-900 bg-transparent shadow-inner shadow-white transition duration-300 hover:bg-white/20">
+      <div class="absolute left-0 top-0 size-full bg-gradient-to-b from-transparent to-gray-800" />
       <Show when={props.imageSrc}>
-        <img src={props.imageSrc} alt={props.title} />
+        <img
+          src={props.imageSrc}
+          alt={props.title}
+          class="-z-10 size-full object-cover transition group-hover/card:scale-105"
+        />
       </Show>
-      <p class="relative z-10 truncate text-xl font-bold text-gray-50 md:text-2xl">
+      <p class="absolute bottom-0 left-0 z-10 truncate p-4 text-xl font-bold text-gray-50 drop-shadow md:text-2xl">
         {props.title}
       </p>
     </div>
--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -4,7 +4,8 @@ import { Card } from "../components/Card";
 
 const items = Array.from({ length: 6 }, (_, index) => {
   return {
-    imageSrc: "",
+    imageSrc:
+      "https://i.pinimg.com/736x/bf/95/34/bf953419d76bf747cba69b55e6e03957.jpg",
     title: `item ${index}`,
   };
 });
@@ -13,7 +14,7 @@ const Home: VoidComponent = () => {
   return (
     <div class="flex flex-col items-center gap-6 p-6">
       <div class="container">
-        <div class="grid grid-cols-4 gap-6">
+        <div class="grid gap-8 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5">
           <For each={items}>
             {(item) => <Card imageSrc={item.imageSrc} title={item.title} />}
           </For>
