7ac5f148ca28e0369c8b384c9457627b6c638b5f
Nir Tamir <nirtamir2@gmail.com>
2024-10-16 18:44:13 +0300

    animations

--- src/components/_TierList2.tsx
+++ src/components/_TierList2.tsx
@@ -36,7 +36,7 @@ const Sortable = (props: {
     <div
       use:sortable
       class={clsx(
-        "relative flex h-full transition-all",
+        "relative flex h-full transition-[transform,_opacity] will-change-[transform,_opacity]",
         // sortable.isActiveDraggable && !sortable.isActiveDraggable() && "hidden",
         sortable.isActiveDraggable && "opacity-20",
         // isDebug && [
